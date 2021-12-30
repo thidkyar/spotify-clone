4d0fb7122d26dc5b238ee0bd94612af956259f37
Taranvir Hidkyar <thidkyar@gmail.com>
2021-12-29 20:52:44 -0500

    add animations to loginscreen

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,35 @@
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    content: [
+        "./pages/**/*.{js,ts,jsx,tsx}",
+        "./components/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {
+            keyframes: {
+                "fade-in-down": {
+                    "0%": {
+                        opacity: "0",
+                        transform: "translateY(-10px)",
+                    },
+                    "100%": {
+                        opacity: "1",
+                        transform: "translateY(0)",
+                    },
+                },
+                wiggle: {
+                    "0%, 100%": {
+                        transform: "rotate(-3deg)",
+                    },
+                    "50%": {
+                        transform: "rotate(3deg)",
+                    },
+                },
+            },
+            animation: {
+                "fade-in-down": "fade-in-down 1s ease-out",
+                wiggle: "wiggle 2s ease-in-out infinite",
+            },
+        },
+    },
+    plugins: [],
+};
