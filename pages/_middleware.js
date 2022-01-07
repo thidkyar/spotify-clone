07eafe8d9ecdf3a8823df06458ab5c2ee449fa65
Taranvir Hidkyar <thidkyar@gmail.com>
2022-01-06 20:20:14 -0500

    remove middleware

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -1,24 +1,24 @@
-import { getToken } from "next-auth/jwt";
-import { NextResponse } from "next/server";
+// import { getToken } from "next-auth/jwt";
+// import { NextResponse } from "next/server";
 
-export async function middleware(req, res, next) {
-    //TOKEN WILL EXIST IF USER IS LOGGED IN
-    const token = await getToken({ req, secret: process.env.JWT_SECRET });
+// export async function middleware(req, res, next) {
+//     //TOKEN WILL EXIST IF USER IS LOGGED IN
+//     const token = await getToken({ req, secret: process.env.JWT_SECRET });
 
-    const { pathname } = req.nextUrl;
+//     const { pathname } = req.nextUrl;
 
-    //REDIRECT TO HOME PAGE IF TOKEN EXISTS AND ARE REQUESTING LOGIN PAGE 
-    if (token && pathname === "/login") {
-        return NextResponse.redirect("/");
-    }
+//     //REDIRECT TO HOME PAGE IF TOKEN EXISTS AND ARE REQUESTING LOGIN PAGE 
+//     if (token && pathname === "/login") {
+//         return NextResponse.redirect("/");
+//     }
 
-    //ALLOW TO PASS THROUGH IF true
-    if (pathname.includes("/api/auth") || token) {
-        return NextResponse.next();
-    }
+//     //ALLOW TO PASS THROUGH IF true
+//     if (pathname.includes("/api/auth") || token) {
+//         return NextResponse.next();
+//     }
 
-    //REDIRECT THEM TO LOGIN IF THEY DONT HAVE TOKEN AND ARE REQUESTING A PROTECTED ROUTE
-    if (!token && pathname !== "/login") {
-        return NextResponse.redirect("/login");
-    }
-}
+//     //REDIRECT THEM TO LOGIN IF THEY DONT HAVE TOKEN AND ARE REQUESTING A PROTECTED ROUTE
+//     if (!token && pathname !== "/login") {
+//         return NextResponse.redirect("/login");
+//     }
+// }
